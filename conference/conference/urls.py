4809147b3e6d189acbe137cca29a0f4b96dfbf2c
Richard Cornish <richardcornish@gmail.com>
2015-07-25 21:25:49 -0500

    Added static patterns for local dev

--- conference/conference/urls.py
+++ conference/conference/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .views import HomeView
 
@@ -8,3 +10,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomeView.as_view(), name='home'),
 ]
+
+# Static/media for local development
+if getattr(settings, 'DEBUG', False):
+    urlpatterns += staticfiles_urlpatterns()
