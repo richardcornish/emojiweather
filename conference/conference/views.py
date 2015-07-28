29c7dbeee913e6681fafaf1d0e1e5a20b3726201
Richard Cornish <richardcornish@gmail.com>
2015-07-28 02:11:03 -0500

    Post parameters are in POST dictionary, this is not Flask!

--- conference/conference/views.py
+++ conference/conference/views.py
@@ -23,7 +23,7 @@ class ConferenceView(CsrfExemptMixin, View):
 class WeatherView(CsrfExemptMixin, View):
 
     def post(self, request, *args, **kwargs):
-        body = request.values.get('FromZip', None)
+        body = request.POST.get('FromZip', None)
         # body = request.POST.get('Body', None)
 
         r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s' % body)
