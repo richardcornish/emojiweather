8df633772c5e92b258a04d878604ae05e8137b75
Richard Cornish <richardcornish@gmail.com>
2016-08-18 17:34:28 -0500

    Added local media development URLs

--- conference/conference/urls.py
+++ conference/conference/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
 ]
 
+
 # Static/media for local development
 if getattr(settings, 'DEBUG', False):
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
