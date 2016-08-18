75fdc19cdec81f81c333f611da0f8b1dbae64283
Richard Cornish <richardcornish@gmail.com>
2016-08-18 17:36:11 -0500

    Updated whitenoise settings

--- conference/conference/wsgi.py
+++ conference/conference/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for conference project.
+WSGI config for jobboardscraper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conference.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conference.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
