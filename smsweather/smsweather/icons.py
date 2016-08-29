4ab5eaffdd822763b972c6d6a8380a8ae62ddd43
Richard Cornish <richardcornish@gmail.com>
2016-08-28 22:38:04 -0500

    Apparently I need those zeroes

--- smsweather/smsweather/icons.py
+++ smsweather/smsweather/icons.py
@@ -1,17 +1,17 @@
 conditions = {
     'clear-day': '\u2600',
-    'clear-night': '\u1F30C',
-    'rain': '\u1F327',
+    'clear-night': '\u0001F30C',
+    'rain': '\u0001F327',
     'snow': '\u2744',
-    'sleet': '\u1F328',
-    'wind': '\u1F32C',
-    'fog': '\u1F32B',
+    'sleet': '\u0001F328',
+    'wind': '\u0001F32C',
+    'fog': '\u0001F32B',
     'cloudy': '\u2601',
     'partly-cloudy-day': '\u26C5',
-    'partly-cloudy-night': '\u2601\u1F30C',
-    'hail': '\u1F327',
+    'partly-cloudy-night': '\u2601\u0001F30C',
+    'hail': '\u0001F327',
     'thunderstorm': '\u26C8',
-    'tornado': '\u1F32A',
+    'tornado': '\u0001F32A',
 }
 
 moons = [{
@@ -19,26 +19,26 @@ moons = [{
     'emoji': '¯\_(ツ)_/¯'
 }, {
     'phase': 'New moon',
-    'emoji': '\u1F311'
+    'emoji': '\u0001F311'
 }, {
     'phase': 'Waxing Crescent',
-    'emoji': '\u1F312'
+    'emoji': '\u0001F312'
 }, {
     'phase': 'First Quarter',
-    'emoji': '\u1F313'
+    'emoji': '\u0001F313'
 }, {
     'phase': 'Waxing Gibbous',
-    'emoji': '\u1F314'
+    'emoji': '\u0001F314'
 }, {
     'phase': 'Full Moon',
-    'emoji': '\u1F315'
+    'emoji': '\u0001F315'
 }, {
     'phase': 'Waning Gibbous',
-    'emoji': '\u1F316'
+    'emoji': '\u0001F316'
 }, {
     'phase': 'Last Quarter',
-    'emoji': '\u1F317'
+    'emoji': '\u0001F317'
 }, {
     'phase': 'Waning Crescent',
-    'emoji': '\u1F318'
+    'emoji': '\u0001F318'
 }]
