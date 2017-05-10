f1c302eed4eea08979e7cc29c6017c9da30eec11
Richard Cornish <richardcornish@gmail.com>
2017-05-10 17:29:02 -0500

    Prefer single quotes when possible

--- smsweather/smsweather/views.py
+++ smsweather/smsweather/views.py
@@ -87,7 +87,7 @@ class SmsView(CsrfExemptMixin, View):
                     response.message(self.errors['unknown_error'])
 
         except Exception as error:
-            response.message("%s %s" % (self.errors['unknown_error'], error))
+            response.message('%s %s' % (self.errors['unknown_error'], error))
         return HttpResponse(response, content_type='text/xml')
 
 
