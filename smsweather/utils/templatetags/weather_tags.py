7e46c83728ee25357d137fb39b0b16c15447e7fc
Richard Cornish <richardcornish@gmail.com>
2017-11-04 17:09:26 -0500

    Refactored emoji into JSON

--- smsweather/utils/templatetags/weather_tags.py
+++ smsweather/utils/templatetags/weather_tags.py
@@ -1,106 +1,57 @@
+import json
+import os
+
 from django import template
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 register = template.Library()
 
 
 @register.filter
 def iconify(value):
-    icons = {
-        'clear-day': '\u2600',
-        'clear-night': '\U0001F30C',
-        'rain': '\U0001F327',
-        'snow': '\u2744',
-        'sleet': '\U0001F328',
-        'wind': '\U0001F32C',
-        'fog': '\U0001F32B',
-        'cloudy': '\u2601',
-        'partly-cloudy-day': '\u26C5',
-        'partly-cloudy-night': '\u2601\U0001F30C',
-        'hail': '\U0001F327',
-        'thunderstorm': '\u26C8',
-        'tornado': '\U0001F32A',
-    }
-    return icons.get(value, '¯\_(ツ)_/¯')
+    path = os.path.join(BASE_DIR, 'data', 'icons.json')
+    with open(path, 'r') as f:
+        data = json.load(f)
+        return data[value] or data['unknown']
 
 
 @register.filter
 def moonify(value):
-    phases = {
-        'new-moon': {
-            'name': 'New moon',
-            'icon': '\U0001F311',
-        },
-        'waxing-crescent': {
-            'name': 'Waxing Crescent',
-            'icon': '\U0001F312',
-        },
-        'first-quarter': {
-            'name': 'First Quarter',
-            'icon': '\U0001F313',
-        },
-        'waxing-gibbous': {
-            'name': 'Waxing Gibbous',
-            'icon': '\U0001F314',
-        },
-        'full-moon': {
-            'name': 'Full Moon',
-            'icon': '\U0001F315',
-        },
-        'waning-gibbous': {
-            'name': 'Waning Gibbous',
-            'icon': '\U0001F316',
-        },
-        'last-quarter': {
-            'name': 'Last Quarter',
-            'icon': '\U0001F317',
-        },
-        'waning-crescent': {
-            'name': 'Waning Crescent',
-            'icon': '\U0001F318',
-        },
-        'unknown': {
-            'name': 'Unknown moon',
-            'icon': '¯\_(ツ)_/¯',
-        },
-    }
-    percentage = value * 100
-    if 0 <= percentage < 6.25 or 93.75 <= percentage <= 100:
-        return phases['new-moon']
-    elif 6.25 <= percentage < 18.75:
-        return phases['waxing-crescent']
-    elif 18.75 <= percentage < 31.25:
-        return phases['first-quarter']
-    elif 31.25 <= percentage < 43.75:
-        return phases['waxing-gibbous']
-    elif 43.75 <= percentage < 56.25:
-        return phases['full-moon']
-    elif 56.25 <= percentage < 68.75:
-        return phases['waning-gibbous']
-    elif 68.75 <= percentage < 81.25:
-        return phases['last-quarter']
-    elif 81.25 <= percentage < 93.75:
-        return phases['waning-crescent']
-    else:
-        return phases['unknown']
+    path = os.path.join(BASE_DIR, 'data', 'moons.json')
+    with open(path, 'r') as f:
+        data = json.load(f)
+        percentage = value * 100
+        if 0 <= percentage < 6.25 or 93.75 <= percentage <= 100:
+            return data['new-moon']
+        elif 6.25 <= percentage < 18.75:
+            return data['waxing-crescent']
+        elif 18.75 <= percentage < 31.25:
+            return data['first-quarter']
+        elif 31.25 <= percentage < 43.75:
+            return data['waxing-gibbous']
+        elif 43.75 <= percentage < 56.25:
+            return data['full-moon']
+        elif 56.25 <= percentage < 68.75:
+            return data['waning-gibbous']
+        elif 68.75 <= percentage < 81.25:
+            return data['last-quarter']
+        elif 81.25 <= percentage < 93.75:
+            return data['waning-crescent']
+        else:
+            return data['unknown']
 
 
 @register.filter
 def flagify(value):
-    countries = [
-        {
-            'code': 'US',
-            'unicode': '\U0001F1FA\U0001F1F8',
-        }, {
-            'code': 'GB',
-            'unicode': '\U0001F1EC\U0001F1E7',
-        }
-    ]
-    for v in value:
-        for key in v:
-            if key == 'types':
-                for t in v['types']:
-                    if t == 'country':
-                        code = v['short_name']
-                        for c in countries:
-                            if c['code'] == code:
-                                return c
+    path = os.path.join(BASE_DIR, 'data', 'flags.json')
+    with open(path, 'r') as f:
+        data = json.load(f)
+        for v in value:
+            for key in v:
+                if key == 'types':
+                    for t in v['types']:
+                        if t == 'country':
+                            code = v['short_name']
+                            for c in data:
+                                if c == code.lower():
+                                    return data[c]
