35e0c95d7a711b197b76a7496238ad3a5f261b4d
Richard Cornish <richardcornish@gmail.com>
2017-11-11 14:41:57 -0600

    Added autocapitalize to form

--- smsweather/search/forms.py
+++ smsweather/search/forms.py
@@ -6,6 +6,7 @@ from smsweather.mixins import WeatherMixin
 class SearchWeatherForm(WeatherMixin, forms.Form):
     q = forms.CharField(label='Query', widget=forms.TextInput(attrs={
         'type': 'search',
+        'autocapitalize': True,
         'autocorrect': 'off',
         'autofocus': True,
         'class': 'form-control input-lg',
