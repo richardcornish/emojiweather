2716cc5289af80741b75c190b3b255dd8baf3d83
Richard Cornish <richardcornish@gmail.com>
2017-11-13 00:59:57 -0600

    Changed back middleware

--- smsweather/smsweather/settings.py
+++ smsweather/smsweather/settings.py
@@ -42,10 +42,9 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.gzip.GZipMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
