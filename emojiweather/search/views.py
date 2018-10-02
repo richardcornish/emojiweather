879430d53088182e68a08cb422afe8547d28575f
Richard Cornish <richardcornish@gmail.com>
2018-10-01 19:27:06 -0500

    Rewrote search view

--- emojiweather/search/views.py
+++ emojiweather/search/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormMixin
+from django.urls import reverse_lazy
 
 from .forms import SearchWeatherForm
 
@@ -7,16 +8,33 @@ from .forms import SearchWeatherForm
 class SearchView(FormMixin, TemplateView):
     form_class = SearchWeatherForm
     template_name = 'search/search.html'
+    query = None
+    results = None
 
     def get(self, request, *args, **kwargs):
-        query = None
-        results = None
-        if 'q' in request.GET:
-            form = self.form_class(request.GET)
-            if form.is_valid():
-                query = form.cleaned_data['q']
-                results = form.get_results(query)
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
         else:
-            form = self.get_form()
-        context = self.get_context_data(form=form, query=query, results=results)
-        return self.render_to_response(context)
+            return self.form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        if 'query' not in kwargs:
+            kwargs['query'] = self.query
+        if 'results' not in kwargs:
+            kwargs['results'] = self.results
+        return super(SearchView, self).get_context_data(**kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(SearchView, self).get_form_kwargs()
+        if self.request.method in ('GET'):
+            if 'q' in self.request.GET and self.request.GET['q'] is not None:
+                kwargs.update({
+                    'data': self.request.GET,
+                })
+        return kwargs
+
+    def form_valid(self, form):
+        self.query = form.cleaned_data['q']
+        self.results = form.get_results(self.query)
+        return self.render_to_response(self.get_context_data(form=form))
