55951e4c96e98d25542700d923b84233ab397d17
Richard Cornish <richardcornish@gmail.com>
2019-01-24 18:11:26 -0600

    Removed old boilerplate URLs

--- emojiweather/emojiweather/urls.py
+++ emojiweather/emojiweather/urls.py
@@ -1,7 +1,5 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 
 from .views import FaviconView, HomeView, RobotsView
@@ -22,6 +20,4 @@ urlpatterns = [
 
 # Static/media for local development
 if getattr(settings, 'DEBUG', False):
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [re_path(r'^__debug__/', include('debug_toolbar.toolbar'))]
