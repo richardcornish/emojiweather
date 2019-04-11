5d318aab36d57151cf5213324bb35afad8abd1b6
Richard Cornish <richardcornish@gmail.com>
2019-04-11 14:42:05 -0500

    Cleaned out urls

--- emojiweather/emojiweather/urls.py
+++ emojiweather/emojiweather/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 from .views import FaviconView, HomeView, RobotsView
 
@@ -20,8 +18,6 @@ urlpatterns = [
 ]
 
 
-# Static/media for local development
 if getattr(settings, 'DEBUG', False):
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [re_path(r'^__debug__/', include('debug_toolbar.toolbar'))]
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
