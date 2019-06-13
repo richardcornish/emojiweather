446aca70f6736d9eb6cd63929a7a420680715802
Richard Cornish <richardcornish@gmail.com>
2019-06-13 15:29:23 -0500

    Removed caching from search view

--- emojiweather/search/views.py
+++ emojiweather/search/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.views.generic.edit import FormView
 
 from .forms import SearchWeatherForm
@@ -6,6 +8,19 @@ from .forms import SearchWeatherForm
 class SearchView(FormView):
     form_class = SearchWeatherForm
     template_name = 'search/search.html'
+
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if request.GET:
+            form = self.get_form()
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+        return super().get(request, *args, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -20,12 +35,3 @@ class SearchView(FormView):
             'results': form.get_results(query),
         }
         return self.render_to_response(self.get_context_data(**kwargs))
-
-    def get(self, request, *args, **kwargs):
-        if request.GET:
-            form = self.get_form()
-            if form.is_valid():
-                return self.form_valid(form)
-            else:
-                return self.form_invalid(form)
-        return super().get(request, *args, **kwargs)
